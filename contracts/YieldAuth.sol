e213cb16265bc78661e81e532f27f501d959ae4d
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-02-01 23:05:46 +0000

    fixed external interfaces imports

--- contracts/YieldAuth.sol
+++ contracts/YieldAuth.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.6.10;
 
+import "./interfaces/maker/IDai.sol";
 import "./interfaces/IERC2612.sol";
-import "./interfaces/IDai.sol";
 import "./interfaces/IDelegable.sol";
 
 /// @dev This library encapsulates methods obtain authorizations using packed signatures
