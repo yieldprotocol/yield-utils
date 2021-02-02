5608eb4c5ef6521f6be5934c8fdf10cb10d9d99f
Georgios Konstantopoulos <me@gakonst.com>
2021-02-02 11:16:13 +0200

    test: adjust tests to hardhat

--- test/001_orchestrated.ts
+++ test/001_orchestrated.ts
@@ -1,22 +1,35 @@
-const Orchestrated = artifacts.require('Orchestrated')
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
+import OrchestratedArtifact from '../artifacts/contracts/Orchestrated.sol/Orchestrated.json'
+import { Orchestrated } from '../typechain/Orchestrated'
 
-// @ts-ignore
-import { expectRevert } from '@openzeppelin/test-helpers'
-import { id } from 'ethers/lib/utils'
-import { assert } from 'chai'
+import { ethers, waffle } from 'hardhat'
+import { id } from '../src'
+import { expect } from 'chai'
+const { deployContract } = waffle
 
-contract('Orchestrated', async (accounts: string[]) => {
-  let [owner, other] = accounts
+describe('Orchestrated', () => {
+  let ownerAcc: SignerWithAddress
+  let owner: string
+  let other: SignerWithAddress
+  let orchestrated: Orchestrated
 
-  let orchestrated: any
+  before(async () => {
+    const signers = await ethers.getSigners()
+    ownerAcc = signers[0]
+    owner = await ownerAcc.getAddress()
+
+    other = signers[1]
+  })
 
   beforeEach(async () => {
-    orchestrated = await Orchestrated.new()
+    orchestrated = (await deployContract(ownerAcc, OrchestratedArtifact, [])) as Orchestrated
   })
 
   it('non-admin cannot orchestrate', async () => {
     const mintSignature = id('mint(address,uint256)')
-    await expectRevert.unspecified(orchestrated.orchestrate(owner, mintSignature, { from: other }))
+    await expect(orchestrated.connect(other).orchestrate(owner, mintSignature)).to.be.revertedWith(
+      'Ownable: caller is not the owner'
+    )
   })
 
   it('can orchestrate', async () => {
--- test/002_decimalMath.ts
+++ test/002_decimalMath.ts
@@ -1,41 +1,42 @@
-const DecimalMath = artifacts.require('DecimalMathMock')
+import DecimalMathArtifact from '../artifacts/contracts/test/DecimalMathMock.sol/DecimalMathMock.json'
+import { DecimalMathMock as DecimalMath } from '../typechain/DecimalMathMock.d'
 
-// @ts-ignore
-import { expectRevert } from '@openzeppelin/test-helpers'
-import { id } from 'ethers/lib/utils'
-import { assert } from 'chai'
+import { ethers, waffle } from 'hardhat'
+const { deployContract } = waffle
+import { expect } from 'chai'
 
-contract('DecimalMath', async (accounts: string[]) => {
-  let [owner] = accounts
+import { ray } from '../src'
 
-  let math: any
+describe('DecimalMath', () => {
+  let math: DecimalMath
 
-  const one = '1000000000000000000000000000'
-  const two = '2000000000000000000000000000'
-  const three = '3000000000000000000000000000'
-  const six = '6000000000000000000000000000'
+  const one = ray('1')
+  const two = ray('2')
+  const three = ray('3')
+  const six = ray('6')
 
-  beforeEach(async () => {
-    math = await DecimalMath.new({ from: owner })
+  before(async () => {
+    const signers = await ethers.getSigners()
+    math = (await deployContract(signers[0], DecimalMathArtifact, [])) as DecimalMath
   })
 
   it('muld', async () => {
-    assert.equal((await math.muld_(two, three)).toString(), six)
+    expect(await math.muld_(two, three)).to.eq(six)
   })
 
   it('divd', async () => {
-    assert.equal((await math.divd_(six, three)).toString(), two)
+    expect(await math.divd_(six, three)).to.eq(two)
   })
 
   it('divdrup', async () => {
-    assert.equal((await math.divdrup_(six, three)).toString(), two)
-    assert.equal((await math.divdrup_(one, three)).toString(), '333333333333333333333333334')
-    assert.equal((await math.divdrup_(1, two)).toString(), '1')
+    expect(await math.divdrup_(six, three)).to.eq(two)
+    expect(await math.divdrup_(one, three)).to.eq('333333333333333333333333334')
+    expect(await math.divdrup_(1, two)).to.eq('1')
   })
 
   it('muldrup', async () => {
-    assert.equal((await math.muldrup_(two, three)).toString(), six)
-    assert.equal((await math.muldrup_('6666666666666666666666666666', '300000000000000000000000000')).toString(), two)
-    assert.equal((await math.muldrup_('1000000000000000000000000001', '1')).toString(), '2')
+    expect(await math.muldrup_(two, three)).to.eq(six)
+    expect(await math.muldrup_('6666666666666666666666666666', '300000000000000000000000000')).to.eq(two)
+    expect(await math.muldrup_('1000000000000000000000000001', '1')).to.eq('2')
   })
 })
