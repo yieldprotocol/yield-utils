43ea3558c547388f4a9f6fd9b79574abc679e9d5
Georgios Konstantopoulos <me@gakonst.com>
2021-02-08 11:39:10 +0200

    chore: remove dist files

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { BigNumber, BigNumberish } from "ethers";
-export declare const wad: (value: BigNumberish) => BigNumber;
-export declare const ray: (value: BigNumberish) => BigNumber;
-export declare const rad: (value: BigNumberish) => BigNumber;
-export declare const id: (signature: string) => string;
-export * as signatures from "./signatures";
